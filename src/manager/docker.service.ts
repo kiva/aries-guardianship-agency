6f08b2f5dbfaa9f13898d3ca9c39ed05669eb775
Matt Raffel <mattr@kiva.org>
2020-07-20 16:37:51 -0600

    removes testing changes that should not be part of commit
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/manager/docker.service.ts
+++ src/manager/docker.service.ts
@@ -79,10 +79,7 @@ export class DockerService implements IAgentManager {
         await container.start();
         // Comment this in if we want to see docker logs here:
         container.attach({stream: true, stdout: true, stderr: true}, (err, stream) => {
-            // stream.pipe(process.stdout);
-            stream.on('data', (chunk: Buffer) => {
-                Logger.log(`we got stuff [${chunk.length}] '${chunk.toString()}'`);
-            })
+            stream.pipe(process.stdout);
         });
         return container.id
     }
