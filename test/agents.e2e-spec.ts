8e313204ba1cb3437ec2ceca7a8a97db736915c2
Matt <52470716+matt-raffel-kiva@users.noreply.github.com>
2020-08-05 10:05:07 -0600

    Circleci project setup (#23)
    * Add .circleci/config.yml
    
    * Fixes tests that were actually broken (does not fix tests that fail because of environment)
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>
    
    * moves tests and creates a smoke test suite
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>
    
    * changes workflow
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>
    
    * removing stuff not needed at this time
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>
    
    * more changes to config
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>
    
    * changing orb to older version
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>
    
    * fixing command
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>
    
    * removing unneeded command
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>
    
    * adds docker image push (requires docker account in build settings, already done)
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>
    
    * adding orb
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>
    
    * adding orb
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- test/agents.e2e-spec.ts
+++ test/agents.e2e-spec.ts
@@ -134,47 +134,4 @@ describe('Agency Integration Tests', () => {
             .send(data)
             .expect(200);
     });
-
-    it('Governance installed data is valid', () => {
-        const agentGovernance: AgentGovernance = new AgentGovernance('doesnt_matter');
-    });
-
-    it('Governance isValidValue detects invalid value', () => {
-        const agentGovernance: AgentGovernance = new AgentGovernance('permissive');
-        expect(false === agentGovernance.isValidValue('bob'));
-    });
-
-    it('Governance validates correctly adjusts invalid value', () => {
-        const data = {
-            default : {
-                all: 'deny'
-            },
-            somethingWrong : {
-                invalidTopic : {
-                    invalidPermission: 'jibberish'
-                }
-            }
-        };
-        const agentGovernance: AgentGovernance = new AgentGovernance('somethingWrong', data);
-        expect(agentGovernance.getPermission('invalidTopic', 'invalidPermission') === 'deny');
-    });
-
-    it('Governance validate correctly adds all', () => {
-        const data = {
-            somethingWrong : {
-                invalidTopic : {
-                    invalidPermission: 'jibberish'
-                }
-            }
-        };
-        const agentGovernance: AgentGovernance = new AgentGovernance('somethingWrong', data);
-        // all, all doesnt exist so it should return default of deny
-        expect(agentGovernance.getPermission('all','all') === 'deny');
-    });
-
-    it('Governance changes once permission to deny on use', () => {
-        const agentGovernance: AgentGovernance = new AgentGovernance('permissive');
-        expect(agentGovernance.getPermission('Permissive', 'invitation') === 'once');
-        expect(agentGovernance.getPermission('Permissive', 'invitation') === 'deny');
-    });
 });
