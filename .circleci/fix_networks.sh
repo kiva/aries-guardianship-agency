b013891d6d30fb8011950dea5cc1f7276e0d4b7b
Matt Raffel <mattr@kiva.org>
2020-08-31 12:35:07 -0600

    moving scripts because....refer to previous commit message
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- .circleci/fix_networks.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -z $(docker network ls --filter name=project_agency-network --format="{{ .Name }}") ] ; then
-     docker network rm project_agency-network ;
-fi
-
-if [ -z $(docker network ls --filter name=agency-network --format="{{ .Name }}") ] ; then
-     docker network create --driver=bridge --subnet=10.0.0.0/24 agency-network ;
-fi
