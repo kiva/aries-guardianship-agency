5241b18b0b83ac913d7adbc08e8a2d401bafb37e
Jacob <jsaur@kiva.org>
2020-11-03 15:08:45 -0800

    Pro 2410 - Add redis support to agency (#90)
    * Add redis support to agency
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * cleanup
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * set ttl to 0
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- src/app/global.cache.module.ts
+++ src/app/global.cache.module.ts
@@ -1,17 +1,33 @@
-import { Global, Module, CacheModule } from '@nestjs/common';
+import { Global, Module, CacheModule, Logger } from '@nestjs/common';
 import * as fsStore from 'cache-manager-fs-hash';
+import * as redisStore from 'cache-manager-redis-store';
 
 /**
- * TODO right now we're using a file system cache for simplicity, at some point we should switch to redis or something
- * TODO we'll probably remove the cache entirely soon
+ * Supports both a redis cache in our deployed environments, and a file system cache for local testing
  */
 @Global()
 @Module({
-    imports: [CacheModule.register({
-        store: fsStore,
-        path:'/tmp/diskcache',
-        ttl: parseInt(process.env.DEFAULT_CACHE_TTL, 10),
-        max: 1000
+    imports: [CacheModule.registerAsync({
+        useFactory: () => {
+            if (process.env.REDIS_ENABLED === 'true') {
+                Logger.log('Using redis cache');
+                return {
+                    store: redisStore,
+                    host: process.env.REDIS_HOST,
+                    auth_pass: process.env.REDIS_PASS,
+                    port: 6379,
+                    ttl: 0
+                };
+            } else {
+                Logger.log('Using file system cache');
+                return {
+                    store: fsStore,
+                    path:'/tmp/diskcache',
+                    ttl: parseInt(process.env.DEFAULT_CACHE_TTL, 10),
+                    max: 1000
+                };
+            }
+        }
     })],
     exports: [CacheModule]
 })
