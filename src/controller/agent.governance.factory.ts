4141f52742a4e18c481550f2ee048b6175957205
Matt Raffel <mattr@kiva.org>
2020-11-11 10:31:03 -0700

    changing governance to use governance code in the aries-controller package.  removing test since it was just goverance stuff
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/controller/agent.governance.factory.ts
+++
@@ -1,8 +0,0 @@
-import { AgentGovernance } from './agent.governance';
-
-export const AgentGovernanceFactory = {
-    provide: 'AGENT_GOVERNANCE',
-    useFactory: () => {
-        return new AgentGovernance(process.env.POLICY_NAME);
-    },
-};
--- src/controller/handler/agent.response.handler.ts
+++
@@ -1,7 +0,0 @@
-
-/*
-   Defines required contract for classes implementing webhook handlers
- */
-export interface IAgentResponseHandler {
-    handlePost(agentUrl: string, agentId: string, adminApiKey: string, route: string, topic: string, body: any): Promise<any>;
-}
