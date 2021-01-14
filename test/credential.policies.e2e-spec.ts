c8413e9adb53ced09e8a12a17ed3d164ae058c68
Jacob Saur <jsaur@kiva.org>
2021-01-14 21:58:23 +0100

    add revoke test
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- test/credential.policies.e2e-spec.ts
+++ test/credential.policies.e2e-spec.ts
@@ -306,6 +306,85 @@ describe('Issue and Prove credentials using policies (e2e)', () => {
             });
     });
 
+    // Testing revocation here because things fail in a strange way when we try to issue again in a new test
+
+    it('issuer revoke credential', async () => {
+        const data = {
+            cred_ex_id: credentialExchangeId,
+            publish: true
+        };
+        return request(issuerUrl)
+        .post(`/revocation/revoke`)
+        .set('x-api-key', issuerApiKey)
+        .send(data)
+        .expect((res) => {
+            try {
+                Logger.warn(`revocation/revoke  result -> ${res.status}`, res.body);
+                expect(res.status).toBe(200);
+            } catch (e) {
+                Logger.warn(`revocation/revoke errored result -> ${res.status}`, res.body);
+                throw e;
+            }
+        });
+    });
+
+    it('prover proves holders credential', async () => {
+        await ProtocolUtility.delay(1000);
+        const data = {
+            connection_id: issuerConnectionId,
+            comment: 'requesting score above 50',
+            proof_request: {
+                name: 'Proof of Score',
+                version: '1.0',
+                requested_attributes: {
+                    'score': {
+                        name: 'score',
+                        restrictions: [
+                            {
+                                cred_def_id: credentialDefinitionId
+                            }
+                        ]
+                    }
+                },
+                requested_predicates: {}
+            }
+        };
+        Logger.warn(`For issuer ${issuerId} /present-proof/send-request body request '${issuerUrl}' -> `, data);
+        return request(issuerUrl)
+            .post('/present-proof/send-request')
+            .send(data)
+            .set('x-api-key', issuerApiKey)
+            .expect((res) => {
+                try {
+                    Logger.warn(`/present-proof/send-request result -> ${res.status}`, res.body);
+                    expect(res.status).toBe(200);
+                    presentationExchangeId = res.body.presentation_exchange_id;
+                } catch (e) {
+                    Logger.warn(`/present-proof/send-request errored result -> ${res.status}`, res.body);
+                    throw e;
+                }
+            });
+    });
+
+    it('verify proof is failed because revoked', async () => {
+        await ProtocolUtility.delay(6000);
+        return request(issuerUrl)
+            .get(`/present-proof/records/${presentationExchangeId}`)
+            .set('x-api-key', issuerApiKey)
+            .expect((res) => {
+                try {
+                    Logger.warn(`${issuerUrl}/present-proof/records/${presentationExchangeId} result -> ${res.status}`, res.body);
+                    expect(res.status).toBe(200);
+                    expect(res.body.state).toBe('verified');
+                    expect(res.body.verified).toBe('false');
+                    expect(res.body.presentation.requested_proof.revealed_attrs.score.raw).toBe('750');
+                } catch (e) {
+                    Logger.warn(`${issuerUrl}/present-proof/records/${presentationExchangeId} errored result -> ${res.status}`, res.body);
+                    throw e;
+                }
+            });
+    });
+
     it('Spin down agent 1', () => {
         const data = {
             agentId: issuerId
