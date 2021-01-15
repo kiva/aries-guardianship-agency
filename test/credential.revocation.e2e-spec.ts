f332b30929503c102fdbf56cdc5242afe2014899
Jacob <jsaur@kiva.org>
2021-01-15 16:24:18 +0100

    Add revocation test (#110)
    * get base tests in
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * add revoke
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * try this
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>
    
    * right tails server
    
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- test/credential.revocation.e2e-spec.ts
+++ test/credential.revocation.e2e-spec.ts
@@ -20,17 +20,15 @@ describe('Issue and Prove credentials using policies (e2e)', () => {
     let holderApiKey;
     let invitation;
     let issuerConnectionId;
-    let holderConnectionId;
     let schemaId;
     let credentialDefinitionId;
     let credentialExchangeId;
     let presentationExchangeId;
     const agentAdminPort = process.env.AGENT_ADMIN_PORT || 5001;
     const hostUrl = 'http://localhost:3010';
-    const schemaName = 'sample_schema';
+    const schemaName = 'revocable_schema';
     const schemaVersion = '1.0';
     const issuerDid = 'Th7MpTaRZVRYnPiabds81Y';
-    const holderDid = 'XTv4YCzYj8jqZgL1wVMGGL';
 
     beforeAll(async () => {
         issuerApiKey = 'adminApiKey';
@@ -45,7 +43,8 @@ describe('Issue and Prove credentials using policies (e2e)', () => {
             walletKey: 'walletId11',
             adminApiKey: issuerApiKey,
             seed: '000000000000000000000000Steward1',
-            did: issuerDid
+            did: issuerDid,
+            useTailsServer: true
         };
         return request(hostUrl)
             .post('/v1/manager')
@@ -59,12 +58,10 @@ describe('Issue and Prove credentials using policies (e2e)', () => {
 
     it('Spin up agent 2 (holder)', async () => {
         const data = {
-            agentId: 'holder',
-            walletId: 'walletId22',
-            walletKey: 'walletId22',
-            adminApiKey: holderApiKey,
-            seed: '000000000000000000000000000ncra1',
-            did: holderDid
+            agentId: 'holder2',
+            walletId: 'walletId222',
+            walletKey: 'walletId222',
+            adminApiKey: holderApiKey
         };
         return request(hostUrl)
             .post('/v1/manager')
@@ -98,7 +95,6 @@ describe('Issue and Prove credentials using policies (e2e)', () => {
             .expect((res) => {
                 expect(res.status).toBe(200);
                 expect(res.body.connection_id).toBeDefined();
-                holderConnectionId = res.body.connection_id;
             });
     });
 
@@ -137,14 +133,15 @@ describe('Issue and Prove credentials using policies (e2e)', () => {
         await ProtocolUtility.delay(1000);
         const data = {
             schema_id: schemaId,
-            support_revocation: false,
-            tag: 'issued_1'
+            support_revocation: true,
+            tag: 'revokable'
         };
         return request(issuerUrl)
             .post('/credential-definitions')
             .send(data)
             .set('x-api-key', issuerApiKey)
             .expect((res) => {
+                Logger.info(`cred def result -> ${res.status}`, res.body)
                 expect(res.status).toBe(200);
                 credentialDefinitionId = res.body.credential_definition_id;
             });
@@ -198,62 +195,33 @@ describe('Issue and Prove credentials using policies (e2e)', () => {
                 expect(res.status).toBe(200);
                 expect(res.body.results.length).toBeGreaterThan(0);
                 const i = res.body.results.length - 1;
-                expect(res.body.results[i].state).toBe('credential_acked');
+                expect(res.body.results[0].state).toBe('credential_acked');
             });
     });
 
-    it('issuer sends second credential using /send-offer', async () => {
-        await ProtocolUtility.delay(5000);
+    it('issuer revoke credential', async () => {
+        await ProtocolUtility.delay(1000);
         const data = {
-            auto_issue: false, // set to false so we can check our governance policy issuer
-            auto_remove: false,
-            comment: 'pleading the 5th',
-            connection_id: issuerConnectionId,
-            cred_def_id: credentialDefinitionId,
-            credential_preview: {
-            '@type': `did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/issue-credential/1.0/credential-preview`,
-                attributes: [
-                    {
-                        name: 'score',
-                        value: '750'
-                    }
-                ]
-            },
-            trace: false
+            cred_ex_id: credentialExchangeId,
+            publish: true
         };
-
-        Logger.warn(`For issuer ${issuerId} issue-credential/send-offer body request '${issuerUrl}' -> `, data);
         return request(issuerUrl)
-            .post('/issue-credential/send-offer')
-            .send(data)
-            .set('x-api-key', issuerApiKey)
-            .expect((res) => {
-                try {
-                    Logger.warn(`issue-credential/send-offer result -> ${res.status}`, res.body);
-                    expect(res.status).toBe(200);
-                    credentialExchangeId = res.body.credential_exchange_id;
-                } catch (e) {
-                    Logger.warn(`issue-credential/send errored result -> ${res.status}`, res.body);
-                    throw e;
-                }
-            });
-    });
-
-    it('Affirm Issuer credential status', async () => {
-        await ProtocolUtility.delay(5000);
-        return request(issuerUrl)
-            .get('/issue-credential/records')
-            .set('x-api-key', holderApiKey)
-            .send(invitation)
-            .expect((res) => {
+        .post(`/revocation/revoke`)
+        .set('x-api-key', issuerApiKey)
+        .send(data)
+        .expect((res) => {
+            try {
+                Logger.warn(`revocation/revoke  result -> ${res.status}`, res.body);
                 expect(res.status).toBe(200);
-                expect(res.body.results.length).toBeGreaterThan(0);
-                const i = res.body.results.length - 1;
-                expect(res.body.results[i].state).toBe('credential_acked');
-            });
+            } catch (e) {
+                Logger.warn(`revocation/revoke errored result -> ${res.status}`, res.body);
+                throw e;
+            }
+        });
     });
 
     it('prover proves holders credential', async () => {
+        await ProtocolUtility.delay(1000);
         const data = {
             connection_id: issuerConnectionId,
             comment: 'requesting score above 50',
@@ -290,7 +258,7 @@ describe('Issue and Prove credentials using policies (e2e)', () => {
             });
     });
 
-    it('verify proof is proved', async () => {
+    it('verify proof is failed because revoked', async () => {
         await ProtocolUtility.delay(6000);
         return request(issuerUrl)
             .get(`/present-proof/records/${presentationExchangeId}`)
@@ -300,7 +268,7 @@ describe('Issue and Prove credentials using policies (e2e)', () => {
                     Logger.warn(`${issuerUrl}/present-proof/records/${presentationExchangeId} result -> ${res.status}`, res.body);
                     expect(res.status).toBe(200);
                     expect(res.body.state).toBe('verified');
-                    expect(res.body.verified).toBe('true');
+                    expect(res.body.verified).toBe('false');
                     expect(res.body.presentation.requested_proof.revealed_attrs.score.raw).toBe('750');
                 } catch (e) {
                     Logger.warn(`${issuerUrl}/present-proof/records/${presentationExchangeId} errored result -> ${res.status}`, res.body);
