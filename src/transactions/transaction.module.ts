ef844ccbcb49141d777d1c57887e3662b9183c9f
Jacob Saur <jsaur@kiva.org>
2021-05-04 13:50:46 +0000

    remove agent caller dependency
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- src/transactions/transaction.module.ts
+++ src/transactions/transaction.module.ts
@@ -1,7 +1,6 @@
 import { Module, HttpModule } from '@nestjs/common';
 import { AgentModule } from 'aries-controller/agent/agent.module';
 import { AgentGovernanceFactory } from 'aries-controller/controller/agent.governance.factory';
-import { AgentCaller} from 'aries-controller/agent/agent.caller';
 import { GlobalCacheModule } from '../app/global.cache.module';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
@@ -19,6 +18,6 @@ import { TransactionService } from './transaction.service';
     providers: [
         AgentGovernanceFactory,
         TransactionService,
-        AgentCaller],
+    ],
 })
 export class TransactionModule {}
