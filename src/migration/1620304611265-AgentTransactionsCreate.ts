d3fe533111fda7b12c5f22d6362b00198605df0c
Matt Raffel <mattr@kiva.org>
2021-07-26 09:29:08 -0400

    initial update
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/migration/1620304611265-AgentTransactionsCreate.ts
+++ src/migration/1620304611265-AgentTransactionsCreate.ts
@@ -5,6 +5,8 @@ export class AgentTransactions1620304611265 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         Logger.info(`AgentTransactions1620304611265 Creating table`);
+        // to avoid problems with differences in currency locale demarcation
+        // save the amount as string.  we are doing any math on it anyways
         await queryRunner.query(`
             CREATE TABLE IF NOT EXISTS agent_transactions (
                 id serial PRIMARY KEY,
@@ -16,6 +18,9 @@ export class AgentTransactions1620304611265 implements MigrationInterface {
                 merkel_order int,
                 merkel_hash varchar(64) NOT NULL,
                 credential_id varchar(64) NULL,
+                type_id varchar(64) NULL,
+                subject_id varchar(64) NULL,
+                amount varchar(64) NULL,
                 transaction_details text NULL
             );`
         );
