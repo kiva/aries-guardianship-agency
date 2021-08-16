abd9aa60a2f865a795776c154f482bb4cae5fabc
Matt Raffel <mattr@kiva.org>
2021-08-16 13:42:20 -0600

    updates AGA to use AgentService for sending basic message.
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/transactions/messaging/report.message.handler.ts
+++ src/transactions/messaging/report.message.handler.ts
@@ -3,13 +3,15 @@ import { TransactionRequest } from 'aries-controller/agent/messaging/transaction
 import { IBasicMessageHandler } from './basic.message.handler';
 import { DataService } from '../persistence/data.service';
 import { ProtocolHttpService } from 'protocol-common/protocol.http.service';
+import { AgentService } from 'aries-controller/agent/agent.service';
 import { AgentTransaction } from '../persistence/agent.transaction';
 import { TxReportResponseDto } from '../dtos/tx.report.response.dto';
 import { TransactionMessageStatesEnum } from './transaction.message.states.enum';
 
 
 export class ReportMessageHandler implements IBasicMessageHandler {
-    constructor(private readonly agentId: string, private readonly adminApiKey, private readonly connectionId: string,
+    constructor(private readonly agentService: AgentService,
+                private readonly agentId: string, private readonly connectionId: string,
                 private readonly dbAccessor: DataService, private readonly http: ProtocolHttpService) {
     }
 
@@ -17,7 +19,7 @@ export class ReportMessageHandler implements IBasicMessageHandler {
         if (message.state === TransactionMessageStatesEnum.STARTED) {
             // TODO could do our own validation tsp id is allowed to request report
             // 1 let system know we acknowledge report request
-            await this.sendTransactionReportMessage(this.agentId, this.adminApiKey, this.connectionId, TransactionMessageStatesEnum.ACCEPTED,
+            await this.sendTransactionReportMessage(this.connectionId, TransactionMessageStatesEnum.ACCEPTED,
                 message.id, message.tdcFspId, '');
             // 2 build the report
             const transactions: AgentTransaction[] = await this.dbAccessor.getAllTransactions(this.agentId);
@@ -38,34 +40,20 @@ export class ReportMessageHandler implements IBasicMessageHandler {
                 });
             }
             // 3 send it out
-            await this.sendTransactionReportMessage(this.agentId, this.adminApiKey, this.connectionId, TransactionMessageStatesEnum.COMPLETED,
+            await this.sendTransactionReportMessage(this.connectionId, TransactionMessageStatesEnum.COMPLETED,
                 message.id, message.tdcFspId, JSON.stringify(reportRecs));
         }
         return false;
     }
 
-    private async sendTransactionReportMessage(agentId: string, adminApiKey: string, connectionId: string,
+    private async sendTransactionReportMessage(connectionId: string,
                                                state: string, id: string, tdcFspId: string, reportData: any): Promise<any> {
-        const url = `http://${agentId}:${process.env.AGENT_ADMIN_PORT}/connections/${connectionId}/send-message`;
         const msg: TransactionRequest<any> = new TransactionRequest<any>({
             id,
             state,
             tdcFspId,
             transactions: reportData
         });
-        const data = { content: JSON.stringify(msg) };
-        const req: any = {
-            method: 'POST',
-            url,
-            headers: {
-                'x-api-key': adminApiKey,
-            },
-            data
-        };
-
-        Logger.debug(`sendTransactionMessage to ${connectionId}`, msg);
-        const res = await this.http.requestWithRetry(req);
-        Logger.debug(`${agentId} sendTransactionMessage results`, res.data);
-        return res.data;
+        return await this.agentService.sendBasicMessage(msg, connectionId);
     }
 }
--- src/transactions/messaging/transaction.message.response.factory.ts
+++ src/transactions/messaging/transaction.message.response.factory.ts
@@ -1,12 +1,14 @@
 import { HttpService, Injectable } from '@nestjs/common';
 import { ProtocolHttpService } from 'protocol-common/protocol.http.service';
 import { Logger } from 'protocol-common/logger';
+import { AgentService } from 'aries-controller/agent/agent.service';
 import { IBasicMessageHandler } from './basic.message.handler';
 import { TransactionMessageTypesEnum } from './transaction.message.types.enum';
 import { GrantMessageHandler } from './grant.message.handler';
 import { TransactionMessageHandler } from './transaction.message.handler';
 import { ReportMessageHandler } from './report.message.handler';
 import { DataService } from '../persistence/data.service';
+
 
 @Injectable()
 export class TransactionMessageResponseFactory {
@@ -23,15 +25,19 @@ export class TransactionMessageResponseFactory {
      * @param connectionId
      * @param messageTypeId
      */
-    public getMessageHandler(agentId: string, adminApiKey: string, connectionId: string, messageTypeId: string): IBasicMessageHandler {
+    public getMessageHandler(agentService: AgentService,
+                             agentId: string,
+                             adminApiKey: string,
+                             connectionId: string,
+                             messageTypeId: string): IBasicMessageHandler {
         Logger.debug(`processing BasicMessage.messageTypeId of ${messageTypeId} `);
         switch (messageTypeId) {
             case TransactionMessageTypesEnum.GRANT:
                 return new GrantMessageHandler(agentId);
             case TransactionMessageTypesEnum.CREDIT_TRANSACTION:
-                return new TransactionMessageHandler(agentId, adminApiKey, connectionId, this.dataService, this.http);
+                return new TransactionMessageHandler(agentService, agentId, connectionId, this.dataService, this.http);
             case TransactionMessageTypesEnum.TRANSACTION_REQUEST:
-                return new ReportMessageHandler(agentId, adminApiKey, connectionId, this.dataService, this.http);
+                return new ReportMessageHandler(agentService, agentId, connectionId, this.dataService, this.http);
             default:
                 Logger.warn(`BasicMessage.messageTypeId of ${messageTypeId} not recognized.`);
                 break;
