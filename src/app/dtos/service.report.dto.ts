8e2aa73af255769236ca6fc4cd8b9fd663ded56f
Matt Raffel <mattr@kiva.org>
2022-04-14 15:49:42 -0600

    pr changes
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/app/dtos/service.report.dto.ts
+++ src/app/dtos/service.report.dto.ts
@@ -1,6 +1,9 @@
 import { IsArray, IsString } from 'class-validator';
 
-
+/**
+ * Please note: this structure is expected to be used across all services
+ * which report statistics
+ */
 export class ServiceReportDto {
     @IsString() serviceName: string;
     @IsString() startedAt: string;
